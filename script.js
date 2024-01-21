7750f7fc40f0ab859806415997f7cf0616589499
Anna Vo <avo12@mail.valenciacollege.edu>
2024-01-21 16:23:58 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Variables
+let firstName = "Anna";
+let lastName = "Vo";
+let age = 18;
+
+// Alerts
+alert("My name is " + firstName + " " + lastName);
+alert("I am " + age + " years old");
+
+// Console log
+console.log("I am having a lot of fun in this class learning about how to use HTML and Javascript! My favorite part so far was learning about how to use GitHub and Git Bash.");
